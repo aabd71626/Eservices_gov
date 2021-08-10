93954390ee78d85a03a1d554c16ae17d04db8ffd
ميسرة عثمان الواوي <50756640+0597621571@users.noreply.github.com>
2021-08-10 10:34:29 +0300

    maysara1.1

---
+++ app/src/main/java/com/eservices/gov/ps/views/fragments/FeePaymentFragment.java
@@ -0,0 +1,57 @@
+package com.eservices.gov.ps.views.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.eservices.gov.ps.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FeePaymentFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FeePaymentFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public FeePaymentFragment() {
+        // Required empty public constructor
+    }
+    // TODO: Rename and change types and number of parameters
+    public static FeePaymentFragment newInstance(String param1, String param2) {
+        FeePaymentFragment fragment = new FeePaymentFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fee_payment, container, false);
+    }
+}
